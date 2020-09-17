783379cd7d01df9351a7126b9e1142c4ebc8ca0b
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-09-17 17:21:44 +0200

    Refactor with @GetMapping

--- demo-spring-mvc/src/main/java/cloud/frizio/web/demo/mvc/controller/CustomerController.java
+++ demo-spring-mvc/src/main/java/cloud/frizio/web/demo/mvc/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cloud.frizio.web.demo.mvc.dao.CustomerDAO;
@@ -17,7 +18,7 @@ public class CustomerController {
   @Autowired
   CustomerDAO customerDao;
   
-  @RequestMapping("/list")
+  @GetMapping("/list")
   public String listCustomers(Model theModel) {
     // Get customers from the dao
     List<Customer> theCustomers = customerDao.getCustomers();
