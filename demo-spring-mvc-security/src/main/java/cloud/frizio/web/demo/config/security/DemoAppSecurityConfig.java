ac5cab642a6bda4807443c635fbce6d285f95a28
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-09-21 14:32:23 +0200

    Create Security Initializer and Configuration

---
+++ demo-spring-mvc-security/src/main/java/cloud/frizio/web/demo/config/security/DemoAppSecurityConfig.java
@@ -0,0 +1,28 @@
+package cloud.frizio.web.demo.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+/**
+ * DemoAppSecurityConfig
+ */
+@Configuration
+@EnableWebSecurity
+public class DemoAppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    
+    org.springframework.security.core.userdetails.User.UserBuilder users = User.withDefaultPasswordEncoder();
+    
+    auth.inMemoryAuthentication()
+      .withUser(users.username("al").password("test123").roles("EMPLOYEE"))
+      .withUser(users.username("bob").password("test123").roles("MANAGER"))
+      .withUser(users.username("admin").password("test123").roles("ADMIN"));
+
+  }
+
+}
---
+++ demo-spring-mvc-security/src/main/java/cloud/frizio/web/demo/config/security/DemoAppSecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package cloud.frizio.web.demo.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class DemoAppSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+  
+  
+}
